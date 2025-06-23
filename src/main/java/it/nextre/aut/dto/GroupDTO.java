31c81a207903ee85f8df91ad4a5f810db535c7a4
salvatore <100683918+SalvatoreScarfone1999@users.noreply.github.com>
2025-06-23 10:33:35 +0200

    decrizione dei gruppi

--- src/main/java/it/nextre/aut/dto/GroupDTO.java
+++ src/main/java/it/nextre/aut/dto/GroupDTO.java
@@ -7,6 +7,7 @@ import java.util.Set;
  * La classe GroupDTO rappresenta un gruppo con un identificatore e un set di ruoli.
  */
 public class GroupDTO extends DTO {
+	private String descrizione;
 
     private Set<RoleDTO> roleDTO;
 
@@ -21,10 +22,12 @@ public class GroupDTO extends DTO {
      *
      * @param id      Identificatore univoco del gruppo.
      * @param roleDTO Set di ruoli associati al gruppo.
+     * @param descrizione del ruolo.
      */
-    public GroupDTO(Long id, Set<RoleDTO> roleDTO) {
+    public GroupDTO(Long id, Set<RoleDTO> roleDTO,String descrizione) {
         this.id = id;
         this.roleDTO = roleDTO;
+        this.descrizione=descrizione;
     }
 
     /**
@@ -53,8 +56,32 @@ public class GroupDTO extends DTO {
     public void setRoleDTO(Set<RoleDTO> roleDTO) {
         this.roleDTO = new HashSet<>(roleDTO);
     }
-
+    
+    
     /**
+     * restituisce la descrizione del ruolo
+     * 
+     * @return String contenente la descrizione
+     */
+
+    public String getDescrizione() {
+		return descrizione;
+	}
+
+    
+    /**
+     * Imposta la descrizione del ruolo
+     * 
+     * @param descrizione da impostare
+     */
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+
+
+
+	/**
      * Classe builder per la costruzione di istanze di GroupDTO.
      */
     public static class GroupDTOBuilder {
@@ -68,6 +95,7 @@ public class GroupDTO extends DTO {
          * Set di ruoli associati al gruppo.
          */
         private Set<RoleDTO> roleDTO;
+        private String descrizione;
 
         /**
          * Imposta l'identificatore del gruppo.
@@ -90,6 +118,10 @@ public class GroupDTO extends DTO {
             this.roleDTO = roleDTO;
             return this;
         }
+        public GroupDTOBuilder descrizione(String descrizione) {
+        	this.descrizione= descrizione;
+        	return this;
+        }
 
         /**
          * Costruisce un'istanza di GroupDTO con i parametri impostati.
@@ -97,7 +129,7 @@ public class GroupDTO extends DTO {
          * @return Nuova istanza di GroupDTO.
          */
         public GroupDTO build() {
-            return new GroupDTO(id, roleDTO);
+            return new GroupDTO(id, roleDTO,descrizione);
         }
     }
 }
